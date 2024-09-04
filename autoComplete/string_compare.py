a77d860def1a1c39bb40ce3cedcf3f5bcec8c37b
Netanel Bondar <82506378+netanel-bond@users.noreply.github.com>
2024-09-04 16:01:31 +0300

    Add files via upload

--- autoComplete/string_compare.py
+++ autoComplete/string_compare.py
@@ -1,63 +1,101 @@
 
-import Levenshtein as ls
+import difflib
+import math
 
-def del_prefix_suffix_insert(input_str: str, actions: list[tuple[str, int, int]]):
 
+def del_prefix_suffix_insert(actions):
     indices_to_del = []
 
     for i, action in enumerate(actions):
 
-        if action[0] == 'insert' and (action[1] == 0 or action[1] >= len(input_str) - 1):
+        if action[0] == 'insert':
+            indices_to_del.append(i)
+        else:
+            break
+    for i, action in zip(range(len(actions) - 1, -1, -1), reversed(actions)):
+        if action[0] == 'insert':
+            indices_to_del.append(i)
+        else:
+            break
+
+    clean_actions = tuple(filter(lambda x: x[0] not in indices_to_del, enumerate(actions)))
+    clean_actions = tuple([element[1] for element in clean_actions])
+
+    return clean_actions
+
+def del_equal(actions):
+    indices_to_del = []
+
+    for i, action in enumerate(actions):
+
+        if action[0] == 'equal':
             indices_to_del.append(i)
 
-    actions = tuple(filter(lambda x: x[0] not in indices_to_del, enumerate(actions)))
-    actions = tuple([element[1] for element in actions])
+    clean_actions = tuple(filter(lambda x: x[0] not in indices_to_del, enumerate(actions)))
+    clean_actions = tuple([element[1] for element in clean_actions])
 
-    return actions
+    return clean_actions
 
+def get_good_letters_num(actions):
+    good_letters_num = 0
+    for action in actions:
+        if action[0] == 'equal':
+            good_letters_num += action[2] - action[1]
 
-def get_cost(input_str: str, original: str) -> int:
+    return good_letters_num
 
-    actions = ls.editops(input_str, original)
+def get_penalty(actions, original):
+    for action in actions:
+        curr_action, edit_index_input = action[0], action[1]
 
-    actions = del_prefix_suffix_insert(input_str, actions)
-
-    good_letters_num = len(input_str) - len(actions)
-
-    cost = good_letters_num * 2
-    print('Base cost: ' + str(cost))
-
-    deduction = 0
-
-    print('Actions: ', end='')
-    print(actions)
-
-    for action, _ in zip(actions, range(len(input_str))):
-
-        current_action, edit_index = action[0], action[1]
-
-        if current_action == 'replace':
-            if edit_index >= 4:      # same penalty for 5th letter and further
-                deduction = 1
+        if curr_action == 'replace':
+            if edit_index_input >= 4:  # same penalty for 5th letter and further
+                return 1
             else:
-                deduction = 5 - edit_index
+                return 5 - edit_index_input
 
-            break
-
-        elif current_action == 'delete' or current_action == 'insert':
-            if edit_index >= 4:      # same penalty for 5th letter and further
-                deduction = 2
+        elif curr_action == 'insert':
+            if edit_index_input >= 4:  # same penalty for 5th letter and further
+                return 2
             else:
-                deduction = 10 - edit_index * 2
+                if original[action[3]] == original[action[4]]:
+                    edit_index_input += 1
 
-            break
+                return 10 - edit_index_input * 2
+
+        elif curr_action == 'delete':
+            if edit_index_input >= 4:  # same penalty for 5th letter and further
+                return 2
+            else:
+                return 10 - (edit_index_input + 1) * 2
+
+    return 0
 
 
-    cost -= deduction
+def get_score(input_str: str, original: str):
 
-    return cost
+    actions = difflib.SequenceMatcher(None, input_str, original).get_opcodes()
 
-s1 = "to ze or no"
+    clean_actions = del_prefix_suffix_insert(actions)
+
+    score = get_good_letters_num(clean_actions) * 2
+
+    clean_actions = del_equal(clean_actions)
+
+    if len(clean_actions) > 1:
+        return -math.inf
+
+    score -= get_penalty(clean_actions, original)
+
+    return score
+
+
+
+s1 = "tobe or no"
 s2 = "word to be or not to be"
 
-print(get_cost(s1, s2))
+print(get_score_dl(s1, s2))
+
+
+
+
